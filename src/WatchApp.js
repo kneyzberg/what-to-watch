3d412fc255507e8e83bd6d26af1cb21b3bc2a1db
Kathrin Neyzberg <kneyzberg@gmail.com>
2021-08-30 16:57:10 -0700

    WIP form includes genres from API call and hard coded decades

--- src/WatchApp.js
+++ src/WatchApp.js
@@ -1,27 +1,36 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import BEARER_TOKEN from "./config";
+import WatchForm from "./WatchForm";
+
 function WatchApp() {
   const [movieGenres, setMovieGenres] = useState([]);
-  const BASE_URL = "https://api.themoviedb.org/3/"
-  useEffect(function getMovieGenres(){
+  const [isLoading, setIsLoading] = useState(true);
 
-    async function fetchGenres(){
+  const BASE_URL = "https://api.themoviedb.org/3/"
+  useEffect(function getMovieGenres() {
+
+    async function fetchGenres() {
       const genreRes = await axios.get(`${BASE_URL}/genre/movie/list`, {
         headers: {
           Authorization: `Bearer ${BEARER_TOKEN}`
         }
       });
 
-      setMovieGenres(genreRes.data);
+      setMovieGenres(genreRes.data.genres);
+      setIsLoading(false);
     }
     fetchGenres();
   }, []);
-  
+
+  console.log(movieGenres);
+
   return (
     <div>
       <h1>Welcome to Kathrin's What to Watch App!</h1>
       <h3>Fill out the form below to get movie reccomendations based on your mood!</h3>
+      {isLoading && <div>Loading</div>}
+      {!isLoading && <WatchForm genres={movieGenres} />}
     </div>
   )
 }
--- src/WatchForm.js
+++ src/WatchForm.js
@@ -1,15 +1,40 @@
 import React, { useState } from "react";
 
 
-function WatchForm() {
+function WatchForm({ genres }) {
+  const decadeOptions = [
+    "1920s",
+    "1930s",
+    "1940s",
+    "1960s",
+    "1970s",
+    "1980s",
+    "1990s",
+    "2000s",
+    "2010s",
+    "2020s"
+  ]
   const initialFormData = { genre: "", decade: "" };
 
   const [formData, setFormData] = useState(initialFormData);
 
   return (
-    <div>
-      <h1>Welcome to Kathrin's What to Watch App!</h1>
-      <h3>Fill out the form below to get movie reccomendations based on your mood!</h3>
+    <div>WHAT ARE YOU IN THE MOOD FOR?
+      <form>
+        <div>Genre:
+          <select name="genre" id="genre-pick">
+            <option value=""> Please pick a genre</option>
+            {genres.map(g => <option value={g.id}>{g.name}</option>)}
+          </select>
+        </div>
+        <div> Decade:
+          <select name="decade" id="decade-pick">
+            <option value=""> Please pick a decade</option>
+            {decadeOptions.map(d => <option value={d}>{d}</option>)}
+          </select>
+        </div>
+      </form>
+
     </div>
   )
 }
