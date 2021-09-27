6bceceea9e67371f8fd22ccb5d523892524ccad7
David Zemel <zemel96@gmail.com>
2021-09-27 17:00:57 -0400

    added css for MovieList and MovieCard components

--- src/MovieCard.js
+++ src/MovieCard.js
@@ -1,17 +1,16 @@
 import React from "react";
+import "./MovieCard.css";
 
-function MovieCard({imgUrl, posterSize, movie}){
-
+function MovieCard({ imgUrl, posterSize, movie }) {
   const posterURL = `${imgUrl}/${posterSize}/${movie.poster_path}`;
 
-  return(
-    <div>
-      <div>{movie.title} {movie.release_date}</div>
-      <img src={posterURL} alt={`Poster for ${movie.title}`}/>
-
+  return (
+    <div className="MovieCard-container">
+      <img src={posterURL} alt={`Poster for ${movie.title}`} />
+      <div>{movie.title}</div>
+      <div>{movie.release_date}</div>
     </div>
-    
-  )
+  );
 }
 
 export default MovieCard;
--- src/MovieList.js
+++ src/MovieList.js
@@ -1,9 +1,10 @@
 import React from "react";
 import MovieCard from "./MovieCard";
+import "./MovieList.css";
 
 function MovieList({ imgUrl, posterSize, movies }) {
   return (
-    <div>
+    <div className="MovieList-container MovieList-shrink">
       {movies.map((m) => (
         <MovieCard
           key={m.id}
